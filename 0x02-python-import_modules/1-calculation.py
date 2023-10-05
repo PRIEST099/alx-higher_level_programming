37081ff039dc07da5ea1e74a8089dfb65ebd11f6
Ahadi <ahadic044@gmail.com>
2023-10-05 06:14:03 -0700

    task 2 error correction

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import calculator_1 as alx
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, alx.add(a, b)))
-    print("{} - {} = {}".format(a, b, alx.sub(a, b)))
-    print("{} * {} = {}".format(a, b, alx.mul(a, b)))
-    print("{} / {} = {}".format(a, b, alx.div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
