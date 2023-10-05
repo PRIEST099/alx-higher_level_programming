2903c03cc296fb50f4178a9e888995ec18ebbeaf
Ahadi <ahadic044@gmail.com>
2023-10-05 07:01:27 -0700

    #pythoniscool

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
-sum = 0
-length = len(argv)
-if length == 1:
+if __name__ == "__main__":
     sum = 0
-else:
-    for i in range(1, length):
-        sum += int(argv[i])
-print("{}".format(sum))
+    length = len(argv)
+    if length == 1:
+        sum = 0
+    else:
+        for i in range(1, length):
+            sum += int(argv[i])
+    print("{}".format(sum))
