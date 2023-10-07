453a4d97f55716760717f5a631a4d933bb0df94d
Ahadi <ahadic044@gmail.com>
2023-10-07 05:36:34 -0700

    cpython complete task

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,8 +1,4 @@
 #include <Python.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-
 
 /**
  * print_python_list_info - does exactly like the name suggests
@@ -12,7 +8,17 @@
 
 void print_python_list_info(PyObject *p)
 {
-	if (PyListCheck(p))
-		print("python list object found\n");
-	return 0;
+	int i;
+	int size, memory;
+	PyObject *object;
+
+	size = PyList_Size(p);
+	memory = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", memory);
+	for (i = 0; i < size; i++)
+	{
+		object = PyList_GetItem(p, i);
+		printf("Element %d: %s\n", i, Py_TYPE(object)->tp_name);
+	}
 }
