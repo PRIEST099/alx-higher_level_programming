3db65fe0fb675cc659ad339b9ef8031f2ff8c671
Ahadi <ahadic044@gmail.com>
2023-10-23 07:54:42 -0700

    correction of exception handling on task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    ret = 0
-    for i in range(0, x - 1):
+    elements = 0
+
+    for i in my_list:
         try:
-            print("{}".format(my_list[i]), end="")
-            ret += 1
-        except IndexError:
-            pass
-        except:
-            print("{}".format(my_list[ret]), end="")
-            ret +=1
-    print("")
-    return ret
+            if elements >= x:
+                break
+            print(i, end="")
+            elements += 1
+        except Exception:
+            break;
+    print()
+    return elements
