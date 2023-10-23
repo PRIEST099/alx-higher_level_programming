6c2a2149e69f23430766f56c71c1e9b160544994
Ahadi <ahadic044@gmail.com>
2023-10-23 07:56:35 -0700

    pycodestyle for task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,6 +10,6 @@ def safe_print_list(my_list=[], x=0):
             print(i, end="")
             elements += 1
         except Exception:
-            break;
+            break
     print()
     return elements
