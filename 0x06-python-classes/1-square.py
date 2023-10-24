4f00c8bcae27e00c2b009921398b03fd4aff31c5
Ahadi <ahadic044@gmail.com>
2023-10-24 14:39:00 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+class Square:
+    def __init__(self, size):
+        """constructor object.
+        Args:
+        size: length of the side of the square
+        """
+        self.__size = size
