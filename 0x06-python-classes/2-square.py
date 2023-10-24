b2cc9ad454e62cccca1e9f753383479e983fe4fe
Ahadi <ahadic044@gmail.com>
2023-10-24 14:47:34 -0700

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module interpreter"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size=0):
+        """constructor object.
+        Args:
+        size: length of the side of the square
+        """
+        if isInstance(int, size):
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raies ValueError("size must be >= 0")
