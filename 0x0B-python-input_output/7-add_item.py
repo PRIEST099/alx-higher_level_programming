3917abcf2fba0119c822f31fff96c7bea07a3e33
Ahadi <ahadic044@gmail.com>
2023-11-07 12:10:05 -0800

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a module to add all arguments to a json script"""
+import sys
+
+
+if __name__ == "__main__":
+    save_json = __import__('5-save_to_json_file').save_to_json_file
+    load_json = __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_items.json")
