2c5368236ed573c9ceb07301f2446b5fbdaaa169
Ahadi <ahadic044@gmail.com>
2023-11-09 19:36:01 +0200

    pycodestyle for test cases for instantiation of base class

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -2,31 +2,66 @@
 """a unittest for base class"""
 import unittest
 import os
-import sys 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import sys
 from models.base import Base
 
 
 class TestBaseClass(unittest.TestCase):
     """a test class for base class"""
 
-    def test_default_id(self):
-        obj = Base()
-        self.assertEqual(obj.id, 1)
+    def test_default(self):
+        a1 = Base()
+        a2 = Base()
+        self.assertEqual(a1.id, a2.id - 1)
+
+    def test_multiple_objects(self):
+        a1 = Base()
+        a2 = Base()
+        a3 = Base()
+        self.assertEqual(a1.id, a3.id - 2)
+
+    def test_id_to_none(self):
+        a1 = Base(None)
+        a2 = Base(None)
+        self.assertEqual(a1.id, a2.id - 1)
 
     def test_custom_id(self):
-        obj = Base(5)
-        self.assertEqual(obj.id, 5)
+        a1 = Base(10)
+        self.assertEqual(a1.id, 10)
 
-    def test_id_increment(self):
-        obj1 = Base()
-        obj2 = Base()
-        self.assertEqual(obj1.id, 1)
-        self.assertEqual(obj2.id, 2)
+    def test_mixing_deafult_and_custom(self):
+        a1 = Base()
+        a2 = Base(10)
+        a3 = Base()
+        self.assertEqual(a1.id, a3.id - 1)
 
-    def test_setting_to_none(self):
-        obj = Base(None)
-        self.assertEqual(obj.id, 1)
+    def test_changing_the_id(self):
+        a = Base(10)
+        a.id = 100
+        self.assertEqual(a.id, 100)
+
+    def test_string_id(self):
+        self.assertEqual("Ahadi", Base("Ahadi").id)
+
+    def test_float_id(self):
+        self.assertEqual(2.3, Base(2.3).id)
+
+    def test_dictionary_id(self):
+        self.assertEqual({"a": 1, "b": 2}, Base({"a": 1, "b": 2}).id)
+
+    def test_complex_number_id(self):
+        self.assertEqual(complex(23), Base(complex(23)).id)
+
+    def test_boolean_id(self):
+        self.assertEqual(True, Base(True).id)
+
+    def test_range_id(self):
+        self.assertEqual(range(4), Base(range(4)).id)
+
+    def test_many_args_id(self):
+        with self.assertRaises(TypeError):
+            Base(1, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""a test module for rectangle class"""
+import unittest
+import sys
+from models.base import Base
+from models.rectangle import Rectangle
