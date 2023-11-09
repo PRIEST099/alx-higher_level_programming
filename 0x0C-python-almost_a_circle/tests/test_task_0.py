3f3e51fc6af6a5ab052d1e2eed60c6e850a67ef9
Ahadi <ahadic044@gmail.com>
2023-11-09 07:21:35 -0800

    initial commit and task 0 for creating an initial test

---
+++ 0x0C-python-almost_a_circle/tests/test_task_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a test module for task 0"""
+import unittest
+
+
+class TestFirstTask(unittest.TestCase):
+    def test_one(self):
+        self.assertTrue(True)
+    def test_two(self):
+        self.assertFalse(False)
