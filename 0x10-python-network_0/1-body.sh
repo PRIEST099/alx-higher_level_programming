68cdf3605d344fa545f4580376c283180a877396
Ahadi <ahadic044@gmail.com>
2024-03-05 14:24:31 +0200

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script to print the response body with 200 ok status code
-curl -s -o /dev/stdout -w "%{http_code}" "$1"
+curl -s -o /dev/stdout -w "%{http_code}" "$1" | awk '/200/{body=1; next} body{print}'
