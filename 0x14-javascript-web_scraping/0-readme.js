cf2e832c9673b01b2537abcbca2555b4bf5c8a01
Ahadi <ahadic044@gmail.com>
2024-06-04 23:32:04 +0200

    first task

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const path = process.argv[2];
+if (!path) {
+  process.exit(1);
+}
+fs.readFile(path, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
