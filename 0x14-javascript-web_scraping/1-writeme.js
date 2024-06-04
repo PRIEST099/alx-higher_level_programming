9054dd72cf97217781cef428b17037d3cf3efd78
Ahadi <ahadic044@gmail.com>
2024-06-04 23:57:05 +0200

    writting to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const path = process.argv[2];
-const data = process.argv[3];
-if (!path || !data) process.exit(1);
-fs.writeFile(path, data, 'utf8', (err) => {
-  if (err) console.log(err);
-});
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
